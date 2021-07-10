29bd1627e50e976f653e8d77b7f03c81cb048ddb
DevKurka <kampustelnik@outlook.com>
2021-07-10 14:49:09 +0200

    Initial server commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Renderer
+
+Project we use internally to quickly render tilted maps. Forked version of RoRenderV2 (https://devforum.roblox.com/t/minimap-render-rorenderv2/965827), heavily edited.
+
+Ignoring character on client has been entirely removed therefore character spawning shall be disabled for the tool to work properly. Before starting with render you should also make sure no game script are run and all invisible parts are removed.
+
+This tool will automatically split the rendered part of map into plots (each `PPS * Grid` px wide). That has been made to generate images which will fit Roblox's 1024x1024 decal resolution.
+
+This script does not use electron app. Instead it will listen for streamed data and automatically store image in file when finished.
+
+# Usage
+
+1) Configuring map
+You should set rendered part of map by placing two parts (they may be rotated, but make sure they both have same rotation). The script will then calculate center CFrame by 
+```lua
+Part1.CFrame:Lerp(Part2.CFrame, 0.5)
+```
+and determine how many plots it should render. Plot centered on that point will always have id of `ceil(AllPlots / 2)`.
+
+2) Configuring server
+Enter the server folder and run `npm install` to install all nodejs dependencies. You may configure port, output directory and file extension in the `index.js` script. Use `npm run` to run the server script.
---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const Jimp = require('jimp')
+
+const port = 8081
+const exportPath = './out/'
+const extension = '.png'
+
+const app = express()
+
+let imageData = []
+
+let xSize = 0
+let ySize = 0
+let plot = 0
+let y = 0
+
+const exportImg = (path) => new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-new
+  new Jimp(xSize, ySize, function (err, image) {
+    if (err) reject(err)
+
+    imageData.forEach((data, i) => image.setPixelColor(data, Math.floor(i / xSize), i % xSize))
+
+    image.write(path, (err) => {
+      if (err) reject(err)
+      console.log('SAVED')
+      resolve()
+    })
+  })
+})
+
+app.post('/requests', express.json({ limit: '10mb' }), async (req, res) => {
+  if (req.body[0] === 'RENDER_START') {
+    console.log('Started plot', ++plot, ' / ', req.body[3])
+
+    y = 0
+    xSize = req.body[1]
+    ySize = req.body[2]
+    imageData = []
+    console.log('Starting render', xSize, ySize)
+  } else if (req.body[0] === 'RENDER_END') {
+    console.log('Render ended')
+
+    const fullExportPath = exportPath + plot + extension
+    console.log('Storing image in ' + fullExportPath)
+    await exportImg(fullExportPath)
+  } else {
+    console.log('GOT PACKETS', req.body.length, '(' + y + ' / ' + xSize * ySize + ')')
+    y += req.body.length
+    for (let i = 0; i < req.body.length; i++) imageData.push(Jimp.rgbaToInt(...req.body[i]))
+  }
+  res.send('.')
+})
+app.get('/requests', (req, res) => res.send('OK'))
+
+app.listen(port, 'localhost', () => { console.log(`Renderer is now listening on "http://localhost:${port}/requests".`) })
