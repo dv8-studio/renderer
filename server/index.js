7b6225dfe3e10e8d3dab72f43c42aacb64dd02b1
Etheroit <42714242+Etheroit@users.noreply.github.com>
2022-09-25 16:36:46 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const fastify = require('fastify')()
-const sharp = require('sharp')
+const Jimp = require('jimp')
 
 const port = 8081
 const exportPath = './out/'
@@ -8,20 +8,23 @@ const extension = '.png'
 let render = {
   imageSize: { x: 0, y: 0 },
   plot: 0,
-  nextIndex: 0,
   pixels: 0
 }
 
-const exportImg = (path) => {
-  const image = sharp(render.data, {
-    raw: {
-      width: render.imageSize.x,
-      height: render.imageSize.y,
-      channels: 4
-    }
+const exportImg = (path) => new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-new
+  new Jimp(render.imageSize.x, render.imageSize.y, function (err, image) {
+    if (err) reject(err)
+
+    render.data.forEach((data, i) => image.setPixelColor(data, Math.floor(i / render.imageSize.x), i % render.imageSize.x))
+
+    image.write(path, (err) => {
+      if (err) reject(err)
+      console.log('SAVED')
+      resolve()
+    })
   })
-  image.toFile(path)
-}
+})
 
 fastify.listen({ port }, (err, address) => {
 	if (err) {
@@ -35,27 +38,26 @@ fastify.get("/", (request, reply) => reply.send("Running"))
 
 fastify.post('/start', (request, reply) => {
   render.plot = request.body.plot
-  render.nextIndex = 0
   render.imageSize = request.body.imageSize
   render.pixels = render.imageSize.x * render.imageSize.y
   
   console.log('Started plot', render.plot, ' / ', request.body.allPlots)
-  render.data = new Uint8Array(render.pixels * 4)
+  render.data = []
   console.log('Starting render', render.imageSize.x, render.imageSize.y)
   reply.send('ok')
 })
 
-fastify.post('/end', (request, reply) => {
+fastify.post('/end', async (request, reply) => {
   console.log('Render ended')
   
   const fullExportPath = exportPath + render.plot + extension
   console.log('Storing image in ' + fullExportPath)
-  exportImg(fullExportPath)
+  await exportImg(fullExportPath)
   reply.send('ok')
 })
 
 fastify.post('/data', (request, reply) => {
-  for (let pixelComponent of request.body) render.data[render.nextIndex++] = pixelComponent
-  console.log('GOT PACKETS', request.body.length / 4, '(' + (render.nextIndex / 4) + ' / ' + render.pixels + ')')
+  for (let pixel of request.body) render.data.push(Jimp.rgbaToInt(...pixel))
+  console.log('GOT PACKETS', request.body.length, '(' + render.data.length + ' / ' + render.pixels + ')')
   reply.send('ok')
 })
